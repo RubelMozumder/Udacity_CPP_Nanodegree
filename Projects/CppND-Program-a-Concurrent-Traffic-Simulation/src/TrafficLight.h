830f5ef24dba88bf07826e7ed2fbd6a6d2d3b774
RubelMozumder <rubelmozumder@outlook.com>
2022-07-27 16:47:54 +0200

    Initially all the simulation tasks are succefully working well.

--- Projects/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.h
+++ Projects/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.h
@@ -1,53 +1,73 @@
 #ifndef TRAFFICLIGHT_H
 #define TRAFFICLIGHT_H
 
-#include <mutex>
-#include <deque>
 #include <condition_variable>
+#include <deque>
+#include <mutex>
+
 #include "TrafficObject.h"
 
 // forward declarations to avoid include cycle
 class Vehicle;
 
+// FP.3 Define a class „MessageQueue“ which has the public methods send and
+// receive. Send should take an rvalue reference of type TrafficLightPhase
+// whereas receive should return this type. Also, the class should define an
+// std::dequeue called _queue, which stores objects of type TrafficLightPhase.
+// Also, there should be an std::condition_variable as well as an std::mutex as
+// private members.
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
-// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
-// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
-// Also, there should be an std::condition_variable as well as an std::mutex as private members. 
+enum class TrafficLightPhase { red = 0, green };
 
 template <class T>
-class MessageQueue
-{
-public:
+class MessageQueue {
+ public:
+  void send(T &&msg);
 
-private:
-    
+  T receive();
+
+ private:
+  std::deque<T> _mq;
+  std::mutex _MQmutx;
+  std::condition_variable _MQcon_var;
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
-// The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
-// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
-// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
-// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+// class waitingLight() {}
 
-class TrafficLight
-{
-public:
-    // constructor / desctructor
+// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject.
+// The class shall have the public methods „void waitForGreen()“ and „void
+// simulate()“ as well as „TrafficLightPhase getCurrentPhase()“, where
+// TrafficLightPhase is an enum that can be either „red“ or „green“. Also, add
+// the private method „void cycleThroughPhases()“. Furthermore, there shall be
+// the private member _currentPhase which can take „red“ or „green“ as its
+// value.
 
-    // getters / setters
+class TrafficLight : public TrafficObject {
+ public:
+  // constructor / desctructor
+  TrafficLight();
 
-    // typical behaviour methods
+  void waitForGreen();
+  void simulate();
+  TrafficLightPhase getCurrentPhase();
 
-private:
-    // typical behaviour methods
+  // getters / setters
 
-    // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
-    // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
-    // send in conjunction with move semantics.
+  // typical behaviour methods
 
-    std::condition_variable _condition;
-    std::mutex _mutex;
+ private:
+  // typical behaviour methods
+  void cycleThroughPhases();
+  TrafficLightPhase _currentPhase;
+  // For persistency of red and blue light (in millisecond)
+
+  // FP.4b : create a private member of type MessageQueue for messages of
+  // type TrafficLightPhase and use it within the infinite loop to push each
+  // new TrafficLightPhase into it by calling send in conjunction with move
+  // semantics.
+  MessageQueue<TrafficLightPhase> MQ;
+  std::condition_variable _condition;
+  std::mutex _mutex;
 };
 
 #endif
